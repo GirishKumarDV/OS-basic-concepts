ac72a37c9d6df87538e59a6358aefa73f71c329c
GirishKumarDV <gkdv19@gmail.com>
2022-07-18 13:31:28 +0530

    Changes to predictive parsing

--- predictiveparsing.c
+++ predictiveparsing.c
@@ -1,78 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char table[10][10] = {"NT","a","b","A","aBa","Error","B","@","bB"};
-char buffer[10],stack[10];
+char table[10][10] = {"NT", "a", "b", "A", "aBa", "Error", "B", "@", "bB"};
+char buffer[10], stack[10];
 int top = -1;
-char pop(){
+char pop()
+{
     return stack[top--];
 }
-void push(int e){
+void push(int e)
+{
     stack[++top] = e;
 }
-void display_stack(){
-    int i=top;
-    while(i>=0){
-        printf("%c",stack[i]);
+void display_stack()
+{
+    int i = top;
+    while (i >= 0)
+    {
+        printf("%c", stack[i]);
         i--;
     }
     printf("\n");
 }
-char* parse_table(char stack_top, char input_val){
-    switch(stack_top){
-        case 'A': switch(input_val){
-            case 'a': return table[4];
-            case 'b': return table[5];
+char *parse_table(char stack_top, char input_val)
+{
+    switch (stack_top)
+    {
+    case 'A':
+        switch (input_val)
+        {
+        case 'a':
+            return table[4];
+        case 'b':
+            return table[5];
         }
         break;
 
-        case 'B': switch(input_val){
-            case 'a': return table[7];
-            case 'b': return table[8];
+    case 'B':
+        switch (input_val)
+        {
+        case 'a':
+            return table[7];
+        case 'b':
+            return table[8];
         }
-        default: return table[5];
+    default:
+        return table[5];
     }
 }
 
-int main(){
-    int ptr=0,i=0,j,k,w=0;
+int main()
+{
+    int ptr = 0, i = 0, j, k, w = 0;
     char *str;
-    for(j=0;j<3;j++){
-        for(k=0;k<3;k++){
-            printf("%s\t",table[w++]);
+    for (j = 0; j < 3; j++)
+    {
+        for (k = 0; k < 3; k++)
+        {
+            printf("%s\t", table[w++]);
         }
         printf("\n");
     }
     printf("Enter string\n");
-    scanf("%s",buffer);
-    if(buffer[strlen(buffer)-1] != ';'){
+    scanf("%s", buffer);
+    if (buffer[strlen(buffer) - 1] != ';')
+    {
         printf("\nString should end with:");
         exit(0);
     }
     push('$');
     push('A');
-    while(stack[top] != '$' && (ptr<strlen(buffer))){
-        if(stack[top] == buffer[ptr]){
+    while (stack[top] != '$' && (ptr < strlen(buffer)))
+    {
+        if (stack[top] == buffer[ptr])
+        {
             ptr++;
-            printf("1.Element popped is %c\n",pop());
-        }else if(stack[top]=='@'){
-            printf("2.Element popped is %c\n",pop());
-        }else{
-            str=parse_table(stack[top],buffer[ptr]);
-            if(strcmp(str,"Error")==0){
+            printf("1.Element popped is %c\n", pop());
+        }
+        else if (stack[top] == '@')
+        {
+            printf("2.Element popped is %c\n", pop());
+        }
+        else
+        {
+            str = parse_table(stack[top], buffer[ptr]);
+            if (strcmp(str, "Error") == 0)
+            {
                 printf("Error in parsing\n");
                 break;
             }
-            printf("3.Element popped is %c\n",pop());
-            for(i=strlen(str)-1;i>=0;i--)
-            push(*(str+i));
+            printf("3.Element popped is %c\n", pop());
+            for (i = strlen(str) - 1; i >= 0; i--)
+                push(*(str + i));
         }
         display_stack();
     }
-    if(stack[top]=='$' && buffer[ptr]==';')
-    printf("String is accepted\n");
-    else{
+    if (stack[top] == '$' && buffer[ptr] == ';')
+        printf("String is accepted\n");
+    else
+    {
         printf("String is no accepted\n");
     }
-    return 0; 
+    return 0;
 }
