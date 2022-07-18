f8f399705f98c6426bd2f069a7d0cba1dadaa891
GirishKumarDV <gkdv19@gmail.com>
2022-07-19 02:06:25 +0530

    Shift Reduce Parsing Tweaked

--- srp.c
+++ srp.c
@@ -16,7 +16,7 @@ void check()
             stack[sptr] = '\0';
             sptr--;
             flag = 1;
-            printf("\n$%s\t\t%s$\t\tF->id", stack, ip);
+            printf("\n$%s\t\t%s$\t\tReduce F->id", stack, ip);
         }
         if (stack[sptr] == ')' && stack[sptr - 1] == 'E' && stack[sptr - 2] == '(')
         {
@@ -24,33 +24,33 @@ void check()
             stack[sptr - 1] = '\0';
             sptr -= 2;
             flag = 1;
-            printf("\n$%s\t\t%s$\t\tF->(E)", stack, ip);
+            printf("\n$%s\t\t%s$\t\tReduce F->(E)", stack, ip);
         }
         if (stack[sptr] == 'F' && stack[sptr - 1] == '*' && stack[sptr - 2] == 'T')
         {
             stack[sptr - 1] = '\0';
             sptr -= 2;
             flag = 1;
-            printf("\n$%s\t\t%s$\t\tT->T*F", stack, ip);
+            printf("\n$%s\t\t%s$\t\tReduce T->T*F", stack, ip);
         }
         else if (stack[sptr] == 'F')
         {
             stack[sptr] = 'T';
             flag = 1;
-            printf("\n$%s\t\t%s$\t\tT->F", stack, ip);
+            printf("\n$%s\t\t%s$\t\tReduce T->F", stack, ip);
         }
         if (stack[sptr] == 'T' && stack[sptr - 1] == '+' && stack[sptr - 2] == 'E' && ip[iptr] != '*')
         {
             stack[sptr - 1] = '\0';
             sptr -= 2;
             flag = 1;
-            printf("\n$%s\t\t%s$\t\tE->E+T", stack, ip);
+            printf("\n$%s\t\t%s$\t\tReduce E->E+T", stack, ip);
         }
         else if ((stack[sptr] == 'T' && ip[iptr] == ')') || (stack[0] == 'T' && ip[iptr] == '\0') || (stack[sptr] == 'T' && ip[iptr] == '+'))
         {
             stack[sptr] = 'E';
             flag = 1;
-            printf("\n$%s\t\t%s$\t\tE->T", stack, ip);
+            printf("\n$%s\t\t%s$\t\tReduce E->T", stack, ip);
         }
         if ((stack[sptr] == 'T' && ip[iptr] == '*') || (stack[sptr] == 'E' && ip[iptr] == '+') || (stack[sptr] == 'E' && ip[iptr] == ')') ||
             (stack[sptr] == '(' && ip[iptr] == 'i' && ip[iptr + 1] == 'd') || (stack[sptr] == '(' && ip[iptr] == '(') ||
@@ -75,54 +75,65 @@ void check()
     }
 }
 
-int main(){
+int main()
+{
     printf("\t\t\tSHIFT RECDUCE PARSING\t\t\n\n");
     printf("Grammar\n");
     printf("E->E+T|T\nT->T*F|F\nF->( E )|id\n");
     printf("Enter the input string\n");
-    scanf("%s",ip);
+    scanf("%s", ip);
     printf("\n\nSTACK Implementation Table\n");
     printf("\nSTACK\t\tINPUT\t\tACTION\n");
     printf("_______\t\t_____\t\t______\n");
     printf("\n$\t\t%s$\t\t--");
-    strcpy(action,"Shift ");
-    if(ip[iptr]=='('){
-        temp[0]=ip[iptr];
-        temp[1]='\0';
-    }else{
-        temp[0]=ip[iptr];
-        temp[1]=ip[iptr+1];
-        temp[2]='\0';
+    strcpy(action, "Shift ");
+    if (ip[iptr] == '(')
+    {
+        temp[0] = ip[iptr];
+        temp[1] = '\0';
     }
-    strcat(action,temp);
+    else
+    {
+        temp[0] = ip[iptr];
+        temp[1] = ip[iptr + 1];
+        temp[2] = '\0';
+    }
+    strcat(action, temp);
     len = strlen(ip);
-    for(i=0;i<len;i++){
-        if(ip[iptr]=='i'&&ip[iptr+1]=='d'){
-            stack[sptr]=ip[iptr];
+    for (i = 0; i < len; i++)
+    {
+        if (ip[iptr] == 'i' && ip[iptr + 1] == 'd')
+        {
+            stack[sptr] = ip[iptr];
             sptr++;
-            ip[iptr]=' ';
+            ip[iptr] = ' ';
             iptr++;
             stack[sptr] = ip[iptr];
-            stack[sptr+1]='\0';
-            ip[iptr]=' ';
-            iptr++;
-        }else{
-            stack[sptr]=ip[iptr];
-            stack[sptr+1]='\0';
-            ip[iptr]=' ';
+            stack[sptr + 1] = '\0';
+            ip[iptr] = ' ';
             iptr++;
         }
-        printf("\n$%s\t\t%s$\t\t%s",stack,ip,action);
-        strcpy(action,"Shift ");
-        if(ip[iptr]=='(' || ip[iptr]==')' || ip[iptr]=='+' || ip[iptr]=='*'){
-            temp[0]=ip[iptr];
-            temp[1]='\0';
-        }else{
-            temp[0]=ip[iptr];
-            temp[1]=ip[iptr+1];
-            temp[2]='\0';
+        else
+        {
+            stack[sptr] = ip[iptr];
+            stack[sptr + 1] = '\0';
+            ip[iptr] = ' ';
+            iptr++;
         }
-        strcat(action,temp);
+        printf("\n$%s\t\t%s$\t\t%s", stack, ip, action);
+        strcpy(action, "Shift ");
+        if (ip[iptr] == '(' || ip[iptr] == ')' || ip[iptr] == '+' || ip[iptr] == '*')
+        {
+            temp[0] = ip[iptr];
+            temp[1] = '\0';
+        }
+        else
+        {
+            temp[0] = ip[iptr];
+            temp[1] = ip[iptr + 1];
+            temp[2] = '\0';
+        }
+        strcat(action, temp);
         check();
         sptr++;
     }
